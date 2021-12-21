9b5e7619702e5cf9b0a72eb9710dfc6a1cfd8434
Евгений Сапрыкин <77262638+EugenSap@users.noreply.github.com>
2021-12-21 20:53:29 +0300

    WHTB-4

---
+++ Whtb/Utils/IoC.cs
@@ -0,0 +1,22 @@
+﻿using SimpleInjector;
+
+namespace Whtb.Utils
+{
+    public class IoC
+    {
+        private static Container container;
+
+        public static T GetInstance<T>() where T : class
+        {
+            container ??= new Container();
+            container.GetAllInstances<T>();
+            return container.GetInstance<T>();
+        }
+
+        public static void Register<TService, TImplementation>() where TService : class where TImplementation : class, TService
+        {
+            container ??= new Container();
+            container.Register<TService, TImplementation>();
+        }
+    }
+}
